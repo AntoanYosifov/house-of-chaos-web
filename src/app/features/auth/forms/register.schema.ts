62ef8978b2f8547a9fc202f60a4f45340a611775
AntoanYosifov <tapaktapxaomi@gmail.com>
2025-10-11 16:19:54 +0200

    feat(auth/forms): add register form schema (email + nested passwords)

---
+++ src/app/features/auth/forms/register.schema.ts
@@ -0,0 +1,13 @@
+import {FormSchema, FormType} from "../../../shared/forms";
+
+export const registerSchema: FormSchema = {
+    type: FormType.Register,
+    spec: {
+        email: {kind: 'email', initial: ''},
+        passwords: {
+            password: {kind: 'password', initial: ''},
+            confirmPassword: {kind: 'password', initial: ''}
+        }
+    }
+
+}
