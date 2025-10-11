9d27418d0834e8d39dad8424ce488b7e7fe56ea9
AntoanYosifov <tapaktapxaomi@gmail.com>
2025-10-11 18:58:54 +0200

    feat(forms/validation): add password basic validators

--- src/app/shared/forms/validators.ts
+++ src/app/shared/forms/validators.ts
@@ -11,3 +11,23 @@ export const emailValidator: ValidatorFn = (c) => {
     }
     return emailRegex.test(v) ? null : {email: true}
 }
+
+export const passwordBasicValidators: ValidatorFn[] = [
+    (c) => {
+        const v = c.value as string | null | undefined;
+        return !v || v.length === 0 ? {required: true} : null;
+    },
+    (c) => {
+        const v = c.value as string;
+        return v && v.length < 5 ? {minlength: {requiredLength: 5, actualLength: v.length}} : null;
+    },
+    (c) => {
+        const v = c.value as string;
+        return v && v.length > 20 ? {maxlength: {requiredLength: 20, actualLength: v.length}} : null;
+    },
+    (c) => {
+        const v = c.value as string;
+        return v && !latinAlnumRegex.test(v) ? {pattern: 'latinAlnum'} : null
+    }
+]
+
